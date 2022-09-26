20dd55c99120400ab702e7f8db88af313db8e394
Vadim Gusak <gusakvd@gmail.com>
2022-09-26 11:14:22 +1000

    fix files

--- gendiff/base.py
+++ gendiff/base.py
@@ -7,7 +7,6 @@ def generate_diff(file_path1, file_path2):
     file2 = json.load(open(file_path2))
     data = gendiff.data.create_data(file1, file2)
     result = gendiff.data.compare_data(data)
-    for line in result:
-        print(line)
     result = '\n'.join(result)
+    print(result)
     return result
--- gendiff/data.py
+++ gendiff/data.py
@@ -18,13 +18,22 @@ def compare_data(data):
     for key in data['names']:
         if 'first' in data[key] and 'second' in data[key]:
             if data[key]['first'] == data[key]['second']:
-                result.append(f"    {key}: {data[key]['first']}")
+                result.append(create_line(' ', key, data[key]['first']))
             else:
-                result.append(f"  - {key}: {data[key]['first']}")
-                result.append(f"  + {key}: {data[key]['second']}")
-        if 'first' in data[key] and not 'second' in data[key]:
-            result.append(f"  - {key}: {data[key]['first']}")
-        if not 'first' in data[key] and 'second' in data[key]:
-            result.append(f"  + {key}: {data[key]['second']}")
+                result.append(create_line('-', key, data[key]['first']))
+                result.append(create_line('+', key, data[key]['second']))
+        if 'first' in data[key] and not ('second' in data[key]):
+            result.append(create_line('-', key, data[key]['first']))
+        if not ('first' in data[key]) and 'second' in data[key]:
+            result.append(create_line('+', key, data[key]['second']))
     result.append('}')
     return result
+
+
+def create_line(sign, name, value):
+    if isinstance(value, bool):
+        if value:
+            value = 'true'
+        else:
+            value = 'false'
+    return f"  {sign} {name}: {value}"
