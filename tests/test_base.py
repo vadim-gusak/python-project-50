84b92bc05c765d8f6ef956b697247f7c6fd28b23
Vadim Gusak <gusakvd@gmail.com>
2022-09-26 11:40:55 +1000

    add first tests files

---
+++ tests/test_base.py
@@ -0,0 +1,9 @@
+from gendiff.base import generate_diff
+import pytest
+
+
+def test_generate_diff_flat():
+    path1 = './tests/fixtures/test1.json'
+    path2 = './tests/fixtures/test2.json'
+    with open("./tests/fixtures/result1") as result_file:
+        assert generate_diff(path1, path2) == result_file.read()
