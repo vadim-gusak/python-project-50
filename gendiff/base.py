601066e4b741022cad2fdd36d3da2fa84dc03337
Vadim Gusak <gusakvd@gmail.com>
2022-11-01 12:36:43 +1000

    fix base.py

--- gendiff/base.py
+++ gendiff/base.py
@@ -1,10 +1,9 @@
-from gendiff.data import create_tree
-from gendiff.diff import create_diff
-from gendiff.stylish import prepare_to_print_stylish
-from gendiff.plain import prepare_to_print_plaint
-from gendiff.json import prepare_to_print_json_format
-from gendiff.parser import parse
 from os.path import isfile
+from gendiff.diff import create_diff
+from gendiff.parser import parse
+from formatter.plain import prepare_to_print_plaint
+from formatter.stylish import prepare_to_print_stylish
+from formatter.json import prepare_to_print_json_format
 
 
 def open_(path):
@@ -25,9 +24,7 @@ def open_(path):
 def generate_diff(path_1, path_2, print_format='stylish'):
     data_1 = parse(*open_(path_1))
     data_2 = parse(*open_(path_2))
-    tree_1 = create_tree(data_1)
-    tree_2 = create_tree(data_2)
-    diff = create_diff(tree_1, tree_2)
+    diff = create_diff(data_1, data_2)
     if print_format == 'plain':
         result = prepare_to_print_plaint(diff)
     elif print_format == 'json':
