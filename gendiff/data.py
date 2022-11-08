70de64393ec67f55061d22caaff6fce9520a3d2d
Vadim Gusak <gusakvd@gmail.com>
2022-11-08 15:06:33 +1000

    remove fix value func

--- gendiff/data.py
+++ gendiff/data.py
@@ -1,18 +1,3 @@
-def fix_value(value):
-    if isinstance(value, dict):
-        return value
-    elif isinstance(value, bool):
-        if value:
-            return 'true'
-        else:
-            return 'false'
-    elif value is None:
-        return 'null'
-    elif isinstance(value, str):
-        return value.strip()
-    return value
-
-
 def get_name_type_value(item):
     name = item.get('name')
     type_ = item.get('type')
--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,6 +1,3 @@
-from gendiff.data import fix_value
-
-
 def create_diff(data_1, data_2):
     names_1, names_2 = set(data_1) - set(data_2), set(data_2) - set(data_1)
     common_names = set(data_1) & set(data_2)
@@ -8,17 +5,17 @@ def create_diff(data_1, data_2):
     for name in names_1 | names_2 | common_names:
         new_item = {'name': name}
         if name in names_1:
-            new_item['value'] = fix_value(data_1[name])
+            new_item['value'] = data_1[name]
             new_item['type'] = 'removed'
             result.append(new_item)
             continue
         elif name in names_2:
-            new_item['value'] = fix_value(data_2[name])
+            new_item['value'] = data_2[name]
             new_item['type'] = 'added'
             result.append(new_item)
             continue
-        value_1 = fix_value(data_1[name])
-        value_2 = fix_value(data_2[name])
+        value_1 = data_1[name]
+        value_2 = data_2[name]
         if isinstance(value_1, dict) and isinstance(value_2, dict):
             new_item['value'] = create_diff(value_1, value_2)
             new_item['type'] = 'nested'
