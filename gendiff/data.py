4bdc8bdbd9031fb099a4bfb9baddaf7bcb15f04b
Vadim Gusak <gusakvd@gmail.com>
2022-11-14 18:07:04 +1000

    delete excess funcs

--- gendiff/data.py
+++ gendiff/data.py
@@ -5,13 +5,5 @@ def get_name_type_value(item):
     return name, type_, value
 
 
-def get_type(item):
-    return item.get('type')
-
-
 def get_name(item):
     return item.get('name')
-
-
-def get_value(item):
-    return item.get('value')
