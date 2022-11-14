dcb579aa7432c67c059e1c650f2893647d962229
Vadim Gusak <gusakvd@gmail.com>
2022-11-14 18:07:48 +1000

    updade parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,9 +4,6 @@ import json
 
 def parse(data, data_format):
     if data and data_format == 'json':
-        result = json.loads(data)
+        return json.loads(data)
     elif data and data_format == 'yml':
-        result = yaml.load(data, yaml.Loader)
-    else:
-        result = {}
-    return result
+        return yaml.load(data, yaml.Loader)
